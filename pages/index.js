d6f60969fcee1f182184febce90374bae05e7849
maducphong7@gmail.com <maducphong1998>
2022-11-27 17:55:00 -0500

    completed

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
       <Row style={{marginTop: "20px"}}>
         <Col md={6}>
           <p>
-          Thee Metropolitan Museum of Art of New York City, colloquially &quot;the Met&quot;, is the largest art museum in the Americas. 
+          The Metropolitan Museum of Art of New York City, colloquially &quot;the Met&quot;, is the largest art museum in the Americas. 
           Its permanent collection contains over two million works, divided among 17 curatorial departments. The main building at 1000 Fifth Avenue, along the Museum Mile on the eastern edge of Central Park on Manhattan&apos;s Upper East Side, is by area one of the world&apos;s largest art museums. A much smaller second location, The Cloisters at Fort Tryon Park in Upper Manhattan, 
           contains an extensive collection of art, architecture, and artifacts from medieval Europe.
           </p>
