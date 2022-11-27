cc08fb5ed185ae4d8a42032a4f567159d45a55da
maducphong7@gmail.com <maducphong1998>
2022-11-27 17:54:13 -0500

    tested vercel

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@
 *
 * Name: Duc Phong Ma  Student ID: 137015194  Date: November 30th, 2022
 *
-* Vercel App (Deployed) Link: _____________________________________________________
+* Vercel App (Deployed) Link: https://web-422-assignment4.vercel.app/
 *
 ********************************************************************************/ 
 import { Row, Col } from 'react-bootstrap';
@@ -29,7 +29,7 @@ export default function Home() {
       <Row style={{marginTop: "20px"}}>
         <Col md={6}>
           <p>
-          The Metropolitan Museum of Art of New York City, colloquially &quot;the Met&quot;, is the largest art museum in the Americas. 
+          Thee Metropolitan Museum of Art of New York City, colloquially &quot;the Met&quot;, is the largest art museum in the Americas. 
           Its permanent collection contains over two million works, divided among 17 curatorial departments. The main building at 1000 Fifth Avenue, along the Museum Mile on the eastern edge of Central Park on Manhattan&apos;s Upper East Side, is by area one of the world&apos;s largest art museums. A much smaller second location, The Cloisters at Fort Tryon Park in Upper Manhattan, 
           contains an extensive collection of art, architecture, and artifacts from medieval Europe.
           </p>
