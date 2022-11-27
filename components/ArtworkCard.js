16930c1163f958048c8a4364118ef2aa956dfc2b
maducphong7@gmail.com <maducphong1998>
2022-11-27 18:49:03 -0500

    updated css

--- components/ArtworkCard.js
+++ components/ArtworkCard.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
 
+
 export default function ArtworkCard(props) {
     const { data, error } = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${props.objectID}`);
 
@@ -16,18 +17,19 @@ export default function ArtworkCard(props) {
     else{
         return (
             <>
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem'}}>
                     {data?.primaryImageSmall ? 
-                    <Card.Img variant="top" src={data?.primaryImageSmall} /> :
-                    <Card.Img variant="top" src="https://via.placeholder.com/375x375.png?text=%5b+Not+Available+%5d" />
+                    <Card.Img variant="top" src={data?.primaryImageSmall} style={{ maxHeight: '210px' }}/> :
+                    <Card.Img variant="top" src="https://via.placeholder.com/375x375.png?text=%5b+Not+Available+%5d" style={{ maxHeight: '210px' }}/>
                     }
                     
-                    <Card.Body>
-                        <Card.Title>{data?.title ? data?.title : 'N\/A'}</Card.Title>
-                        <Card.Text>
-                            <strong>Date: </strong> {data?.objectDate ? data?.objectDate : 'N\/A'}<br />
-                            <strong>Classification: </strong> {data?.classification ? data?.classification : 'N\/A'}<br />
-                            <strong>medium: </strong> {data?.medium ? data?.medium : 'N\/A'}<br /><br />
+                    <Card.Body style={{lineHeight: '1em'}}>
+                        <Card.Title style={{ overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '1' }}>
+                        {data?.title ? data?.title : 'N\/A'}</Card.Title>
+                        <Card.Text style={{marginTop: '20px'}}>
+                            <span style={{ overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '1' }}><strong>Date: </strong> {data?.objectDate ? data?.objectDate : 'N\/A'}</span><br />
+                            <span style={{ overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '1' }}><strong>Classification: </strong>{data?.classification ? data?.classification : 'N\/A'}</span><br />
+                            <span style={{ overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: '1' }}><strong>medium: </strong>{data?.medium ? data?.medium : 'N\/A'}</span><br /><br />
                         </Card.Text>
                         <Link href={'/artwork/' + props.objectID} passHref>
                             <Button variant="outline-dark"><strong>ID: </strong>{props.objectID}</Button>
